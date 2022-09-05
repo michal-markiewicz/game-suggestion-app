e04127b4845daee38dbaa575421f138f7375688c
Michał Markiewicz <michalmarkiewicz0953@gmail.com>
2022-09-05 15:19:05 +0200

    Need to commit because im switching OS

---
+++ js/api.js
@@ -0,0 +1,3 @@
+fetch("https://id.twitch.tv/oauth2/token?client_id=kv96kplxw3trd1gietl2xub6oqd09d&client_secret=z914hhfak8kb5vt54i4fjv66m5cqfu&grant_type=client_credentials").then((e) => {
+    console.log(e);
+})
